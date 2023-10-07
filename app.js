f088d6137c5c4f7307ebff029f75c122dec4ef70
Srishti Srivastava <ssrishti508@gmail.com>
2023-10-07 18:51:05 +0530

    post delete and get requests

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const connection = mysql.createConnection({
 
 connection.connect()
 
+app.use(express.json());
+
 
 app.get('/students', (req, res) => {
     connection.query('SELECT * FROM StudentDetail', (err, rows, fields) => {
@@ -23,6 +25,63 @@ app.get('/students', (req, res) => {
       }
     });
   });
+
+app.get('/students/:studentID',(req,res)=>{
+    const id = req.params.studentID;
+
+    connection.query('SELECT * FROM StudentDetail WHERE studentID = ?', [id], (err, rows, fields)=>{
+        if(err){
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else{
+            if (rows.length>0) {
+                res.json(rows[0]);
+            } else {
+                res.status(404).json({ error: 'Student not found' });
+            }
+        }
+    })
+})
+
+
+app.post('/addstudent', (req, res) => {
+    const { StudentID, LastName, FirstName, City, Phone } = req.body;
+  
+    // Perform a database query to insert the new student detail
+    connection.query(
+      'INSERT INTO StudentDetail (StudentID, LastName, FirstName, City, Phone) VALUES (?, ?, ?, ?, ?)',
+      [StudentID, LastName, FirstName, City, Phone],
+      (err, result) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+          res.status(200).json({ message: 'Student detail added successfully' });
+        }
+      }
+    );
+  });
+
+app.delete('/removestudent/:id',(req,res)=>{
+    const studentid = req.params.id;
+
+    connection.query(
+        'DELETE FROM StudentDetail WHERE StudentID = ?',
+        [studentid],
+        (err, result) => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+          } else {
+            if (result.affectedRows > 0) {
+              res.json({ message: `Student with ID ${studentid} deleted successfully` });
+            } else {
+              res.status(404).json({ error: `Student with ID ${studentid} not found` });
+            }
+          }
+        }
+      );
+})
 
 
 const server = app.listen(port,()=>{
